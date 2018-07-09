80717b842d1d853eb1983e915933cf4d29ca5158
latimeks <clatimer@revspringinc.com>
2018-07-09 14:39:41 -0400

    First project and custom component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+// A component is just a typescript class that is used in angular
+// All components need a component decorator this can be done manually or with ng generate component foo or ng gc foo
+import {Component} from '@angular/core'
+@Component({
+  selector:'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
