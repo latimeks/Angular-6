e117c6de7ef1e5a7abbdeb2d9ba2ba6c8805dcad
latimeks <clatimer@revspringinc.com>
2018-07-09 16:05:22 -0400

    Adding the rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component'
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
