bf1311600f8dad0140517fa935c508b55c1e7f46
latimercaleb <nohitokaze@gmail.com>
2018-07-20 02:21:47 -0400

    App 2: 80% complete
    Still need to add unit tests and more styles

--- Practice/App2-Databinding/src/app/app.module.ts
+++ Practice/App2-Databinding/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { UserformComponent } from './userform/userform.component';
@@ -14,7 +18,11 @@ import { UserformComponent } from './userform/userform.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
